ec30cccb63cd3456833e2e5e23c5755b9866fa33
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-25 04:44:57 +0000

    Updates prime app

---
+++ PrimeDirective.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeDirective
+{
+    public class PrimeDirective
+    {
+        public bool isPrime(int number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+            else if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public List<int> OnlyPrimes(int[] numbers)
+        {
+            List<int> primes = new List<int>();
+
+            foreach (int num in numbers)
+            {
+                if (isPrime(num))
+                {
+                    primes.Add(num);
+                }
+            }
+            return primes;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
-﻿namespace PrimeDirective
+﻿using System.Collections;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace PrimeDirective
 {
-    internal class PrimeDirective
+    public class Program
     {
-        public bool isPrime(int number)
-        {
-            if(number == 2)
-            {
-                return true;
-            }
-            else if(number < 2)
-            {
-                return false;
-            }
-
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-
         static void Main(string[] args)
         {
             PrimeDirective pd = new PrimeDirective();
             Console.WriteLine(pd.isPrime(6));
+
+            int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
+
+            Console.WriteLine("List of numbers and their primality:");
+            foreach (int num in numbers)
+            {
+                bool isPrime = pd.isPrime(num);
+                Console.WriteLine($"{num}: {(isPrime ? "Prime" : "Not Prime")}");
+            }
+
+            List<int> primes = pd.OnlyPrimes(numbers);
+
+            Console.WriteLine("\nList of prime numbers:");
+            foreach (int prime in primes)
+            {
+                Console.WriteLine(prime);
+            }
         }
     }
 }
