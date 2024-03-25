1739b542a802b94bf53c27d39079b6054e75dedc
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-25 04:25:07 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PrimeDirective
+{
+    internal class PrimeDirective
+    {
+        public bool isPrime(int number)
+        {
+            if(number == 2)
+            {
+                return true;
+            }
+            else if(number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrimeDirective pd = new PrimeDirective();
+            Console.WriteLine(pd.isPrime(6));
+        }
+    }
+}
